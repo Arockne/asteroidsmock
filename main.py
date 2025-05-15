8d3cd6360fc613f34bc474be19018e9722166a41
Daniel Calvin <daniel.joseph.calvin@gmail.com>
2025-05-15 10:03:16 -0700

    add comments

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ def main():
   print(f"Screen height: {SCREEN_HEIGHT}")
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   while True:
+    #check player inputs
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+    #update game world
     screen.fill("black")
+    #draw game on screen
     pygame.display.flip()
   
 if __name__ == "__main__":
